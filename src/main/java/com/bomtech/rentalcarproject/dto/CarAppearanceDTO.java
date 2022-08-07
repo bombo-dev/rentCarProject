267d03722128147f72c52b65eb0c38d10b53a9e6
Bombo <bombo96@naver.com>
2022-08-07 13:34:54 +0900

    Add CarAppearanceDTO

---
+++ src/main/java/com/bomtech/rentalcarproject/dto/CarAppearanceDTO.java
@@ -0,0 +1,23 @@
+package com.bomtech.rentalcarproject.dto;
+
+public class CarAppearanceDTO {
+
+    private String appearance;
+    private String appearance_num;
+
+    public String getAppearance() {
+        return appearance;
+    }
+
+    public void setAppearance(String appearance) {
+        this.appearance = appearance;
+    }
+
+    public String getAppearance_num() {
+        return appearance_num;
+    }
+
+    public void setAppearance_num(String appearance_num) {
+        this.appearance_num = appearance_num;
+    }
+}
