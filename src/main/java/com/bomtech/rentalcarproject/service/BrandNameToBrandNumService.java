a5f76fb1e1791c58d06652dbb244d15a369e64de
Bombo <bombo96@naver.com>
2022-08-07 16:28:43 +0900

    "modified Many BrandList to OneList"

--- src/main/java/com/bomtech/rentalcarproject/service/BrandNameToBrandNumService.java
+++ src/main/java/com/bomtech/rentalcarproject/service/BrandNameToBrandNumService.java
@@ -6,41 +6,26 @@ import com.bomtech.rentalcarproject.mapper.CarBrandMapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class BrandNameToBrandNumService {
-    private CarBrandMapper carBrandMapper;
+    private final CarBrandMapper carBrandMapper;
 
-//    @Autowired
-    public BrandNameToBrandNumService(CarBrandMapperImpl carBrandMapper){
+    @Autowired
+    public BrandNameToBrandNumService(CarBrandMapper carBrandMapper){
         this.carBrandMapper = carBrandMapper;
     }
 
-    public static List<CarBrandDTO> koreaBrandList;
-    public static List<CarBrandDTO> germanyBrandList;
-    public static List<CarBrandDTO> japenBrandList;
-    public static List<CarBrandDTO> americaBrandList;
-    public static List<CarBrandDTO> italyBrandList;
+    public static List<CarBrandDTO> BrandList = new ArrayList<>();
 
-    public void setList(){
+    public void setBrandList(){
         List<CarBrandDTO> list = carBrandMapper.listBrand();
-        System.out.println("list : " + list.get(0));
+
         for(CarBrandDTO value : list){
-            if(value.getBrand_num().toString().startsWith("0")){
-                koreaBrandList.add(value);
-            }
-            else if(value.getBrand_num().toString().startsWith("1")){
-                germanyBrandList.add(value);
-            }
-            else if(value.getBrand_num().toString().startsWith("2")){
-                japenBrandList.add(value);
-            }
-            else if(value.getBrand_num().toString().startsWith("3")){
-                americaBrandList.add(value);
-            }
-            else if(value.getBrand_num().toString().startsWith("4")){
-                italyBrandList.add(value);
+            if(value.getBrand_num().startsWith("0")){
+                BrandList.add(value);
             }
         }
     }
