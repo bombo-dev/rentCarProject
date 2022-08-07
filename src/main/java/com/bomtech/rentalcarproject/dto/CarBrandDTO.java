f7f92b40470849cbac74ce1b00698ea5392191da
Bombo <bombo96@naver.com>
2022-08-07 13:56:01 +0900

    Rename brand to brand_name

--- src/main/java/com/bomtech/rentalcarproject/dto/CarBrandDTO.java
+++ src/main/java/com/bomtech/rentalcarproject/dto/CarBrandDTO.java
@@ -1,15 +1,15 @@
 package com.bomtech.rentalcarproject.dto;
 
 public class CarBrandDTO {
-    private String brand;
+    private String brand_name;
     private String brand_num;
 
-    public String getBrand() {
-        return brand;
+    public String getBrand_name() {
+        return brand_name;
     }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
+    public void setBrand_name(String brand_name) {
+        this.brand_name = brand_name;
     }
 
     public String getBrand_num() {
