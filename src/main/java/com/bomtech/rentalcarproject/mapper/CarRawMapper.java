73ad99f0fe63335c0e91bf11a95ebf1df656a484
Bombo <bombo96@naver.com>
2022-08-07 13:39:10 +0900

    Add ListRaw Method in CarRawMapper

--- src/main/java/com/bomtech/rentalcarproject/mapper/CarRawMapper.java
+++ src/main/java/com/bomtech/rentalcarproject/mapper/CarRawMapper.java
@@ -1,2 +1,10 @@
-package com.bomtech.rentalcarproject.mapper;public interface CarRawMapper {
+package com.bomtech.rentalcarproject.mapper;
+
+import com.bomtech.rentalcarproject.dto.CarRawDTO;
+
+import java.util.List;
+
+public interface CarRawMapper {
+
+    List<CarRawDTO> listRaw();
 }
