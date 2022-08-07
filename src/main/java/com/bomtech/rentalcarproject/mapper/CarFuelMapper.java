6fbde3128413cda528de887715102876587d6ceb
Bombo <bombo96@naver.com>
2022-08-07 14:18:39 +0900

    Create CarFuelMapper Interface

---
+++ src/main/java/com/bomtech/rentalcarproject/mapper/CarFuelMapper.java
@@ -0,0 +1,10 @@
+package com.bomtech.rentalcarproject.mapper;
+
+import com.bomtech.rentalcarproject.dto.CarFuelDTO;
+
+import java.util.List;
+
+public interface CarFuelMapper {
+
+    List<CarFuelDTO> listFuel();
+}
