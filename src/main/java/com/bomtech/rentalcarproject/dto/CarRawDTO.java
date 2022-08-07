67c7773c52cbc2ddc96d4d0edcb287dbb9c0d4a4
Bombo <bombo96@naver.com>
2022-08-07 13:38:02 +0900

    Create CarRawDTO

---
+++ src/main/java/com/bomtech/rentalcarproject/dto/CarRawDTO.java
@@ -0,0 +1,23 @@
+package com.bomtech.rentalcarproject.dto;
+
+public class CarRawDTO {
+
+    private String raw_name;
+    private String raw_num;
+
+    public String getRaw_name() {
+        return raw_name;
+    }
+
+    public void setRaw_name(String raw_name) {
+        this.raw_name = raw_name;
+    }
+
+    public String getRaw_num() {
+        return raw_num;
+    }
+
+    public void setRaw_num(String raw_num) {
+        this.raw_num = raw_num;
+    }
+}
