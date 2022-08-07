63ffee67ed797f8642976b731b7da9964fe415b2
Bombo <bombo96@naver.com>
2022-08-07 13:34:09 +0900

    Add CarAppearanceMapper Interface

---
+++ src/main/java/com/bomtech/rentalcarproject/mapper/CarAppearanceMapper.java
@@ -0,0 +1,9 @@
+package com.bomtech.rentalcarproject.mapper;
+
+import com.bomtech.rentalcarproject.dto.CarAppearanceDTO;
+
+import java.util.List;
+
+public interface CarAppearanceMapper {
+    List<CarAppearanceDTO> carAppearanceList();
+}
