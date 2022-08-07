f01cc46aa46fdc316c44299102c50acf61f7f21d
Bombo <bombo96@naver.com>
2022-08-07 16:26:05 +0900

    "Add InsertCategory in Category Table Car_Code checked"

--- src/main/java/com/bomtech/rentalcarproject/mapper/CarCategoryMapperImpl.java
+++ src/main/java/com/bomtech/rentalcarproject/mapper/CarCategoryMapperImpl.java
@@ -1,6 +1,10 @@
 package com.bomtech.rentalcarproject.mapper;
 
-import com.bomtech.rentalcarproject.dto.CarCategoryDTO;
+import com.bomtech.rentalcarproject.dto.*;
+import com.bomtech.rentalcarproject.service.AppearanceNameToAppearanceNumService;
+import com.bomtech.rentalcarproject.service.BrandNameToBrandNumService;
+import com.bomtech.rentalcarproject.service.FuelNameToFuelNumService;
+import com.bomtech.rentalcarproject.service.RawNameToRawNumService;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +20,53 @@ public class CarCategoryMapperImpl implements CarCategoryMapper{
     private SqlSession sqlSession;
 
     public int insertCategory(CarCategoryDTO dto) {
+
+        String brand_code = "";
+        String appearance_code = "";
+        String raw_code = "";
+        String fuel_code = "";
+        String lastCarCode;
+
+        for(CarBrandDTO brand : BrandNameToBrandNumService.BrandList){
+            if(brand.getBrand_name().equals(dto.getBrand())){
+                brand_code = brand.getBrand_num();
+            }
+        }
+
+        for(CarAppearanceDTO appearance : AppearanceNameToAppearanceNumService.carAppearanceList){
+            if(appearance.getAppearance_name().equals(dto.getAppearance())){
+                appearance_code = appearance.getAppearance_num();
+            }
+        }
+
+        for(CarRawDTO raw : RawNameToRawNumService.carRawList){
+            if(raw.getRaw_name().equals(dto.getUse())){
+                raw_code = raw.getRaw_num();
+            }
+        }
+
+        for(CarFuelDTO fuel : FuelNameToFuelNumService.carFuelList){
+            if(fuel.getFuel_name().equals(dto.getFuel())){
+                fuel_code = fuel.getFuel_num();
+            }
+        }
+
+        String checkCode = brand_code + appearance_code + raw_code + fuel_code;
+
+        List<String> sameCarList = sqlSession.selectList("checkCarCode", checkCode);
+        System.out.println(sameCarList);
+        if(sameCarList.size()==0){
+            lastCarCode = checkCode + "00";
+        } else{
+            String value = sameCarList.get(sameCarList.size() - 1);
+            value = value.substring(value.length()-2, value.length());
+            int toValue = Integer.parseInt(value) + 1;
+            String lastValue = String.format("%02d", toValue);
+            lastCarCode = checkCode + lastValue;
+        }
+
+        dto.setCar_code(lastCarCode);
+
         return sqlSession.insert("insertCategory", dto);
     }
 
@@ -29,7 +80,7 @@ public class CarCategoryMapperImpl implements CarCategoryMapper{
         map.put("search", search);
         map.put("searchString", searchString);
 
-        return sqlSession.selectOne("findCategory", map);
+        return sqlSession.selectList("findCategory", map);
     }
 
     public int deleteCategory(int cate_code){
