c3facbfda85e719ca95b22a5334a706460365583
Bombo <bombo96@naver.com>
2022-08-07 13:59:58 +0900

    Create CarFuelDTO

---
+++ src/main/java/com/bomtech/rentalcarproject/dto/CarFuelDTO.java
@@ -0,0 +1,22 @@
+package com.bomtech.rentalcarproject.dto;
+
+public class CarFuelDTO {
+    private String fuel_name;
+    private String fuel_num;
+
+    public String getFuel_name() {
+        return fuel_name;
+    }
+
+    public void setFuel_name(String fuel_name) {
+        this.fuel_name = fuel_name;
+    }
+
+    public String getFuel_num() {
+        return fuel_num;
+    }
+
+    public void setFuel_num(String fuel_num) {
+        this.fuel_num = fuel_num;
+    }
+}
